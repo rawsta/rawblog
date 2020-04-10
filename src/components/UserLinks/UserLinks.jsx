c2c4ef6573c79f988b37a3149edce9317866aee0
Sebastian Fiele <rawsta@rawsta.de>
2020-04-10 19:53:59 +0200

    fixed console.error - key must be on first element in return

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -7,10 +7,10 @@ class UserLinks extends Component {
     const { userLinks } = thisProps.config;
     const { labeled } = thisProps;
     return userLinks.map(link => (
-      <a href={link.url}>
+      <a href={link.url} key={link.label}>
         <button type="button" key={link.label}>
           <i className={link.iconClassName} />
-&nbsp;
+          &nbsp;
           {labeled ? link.label : ""}
         </button>
       </a>
