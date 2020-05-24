80109944b6d4289e67e56e069b876b5d22ed8ae1
Sebastian Fiele <rawsta@rawsta.de>
2020-05-24 14:37:35 +0200

    general updates after revival.. woohoo

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,6 +4,7 @@ import config from "../../data/SiteConfig";
 import Navigation from "../components/Navigation/Navigation";
 import ThemeContext from '../context/ThemeContext';
 import '../../static/assets/css/all.min.css';
+import favicon from '../../static/assets/favicon.ico';
 import "./index.css";
 
 export default class MainLayout extends React.Component {
@@ -19,7 +20,7 @@ export default class MainLayout extends React.Component {
             <Helmet>
               <html lang="de" data-theme={theme.dark ? 'dark' : 'light'}/>
               <meta name="description" content={config.siteDescription} />
-              {/* <link rel="shortcut icon" type="image/png" href={favicon} /> */}
+              <link rel="shortcut icon" type="image/png" href={favicon} />
               <link
                 href="https://fonts.googleapis.com/css?family=Exo+2:wght@300;400;700;900&family=Lato:wght@400;700&display=swap"
                 rel="stylesheet"
