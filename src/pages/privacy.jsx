d8b5975d23d79d1e94213c4ac81de0291ef6da06
rawsta <rawsta@rawsta.de>
2022-05-11 19:39:59 +0200

    fixed text

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -12,25 +12,23 @@ export default class Privacy extends React.Component {
         return (
           <Layout>
             <Helmet>
-              <title>{`${config.siteTitle} | Datenschutzerklärung`}</title>
+              <title>{`${config.siteTitle} | Datenschutzerkl&auml;rung`}</title>
             </Helmet>
             <SEO />
             <div className="page-wrap">
               <section className="page">
                 <div className="page-content">
                   <header className="page-header">
-                    <h1>Datenschutzerklärung</h1>
+                    <h1>Datenschutzerkl&auml;rung</h1>
                   </header>
                   <p>
-                    Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz „Daten“) im Rahmen der Erbringung unserer Leistungen sowie innerhalb unseres Onlineangebotes und der mit ihm verbundenen Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unser Social Media Profile auf (nachfolgend gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO).
+                    Diese Datenschutzerkl&auml;rung kl&auml;rt Sie &uuml;ber die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz „Daten“) im Rahmen der Erbringung unserer Leistungen sowie innerhalb unseres Onlineangebotes und der mit ihm verbundenen Webseiten, Funktionen und Inhalte sowie externen Onlinepr&auml;senzen, wie z.B. unser Social Media Profile auf (nachfolgend gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO).
                   </p>
                   <div className="one-half first">
                     <h4>Verantwortlicher</h4>
-                    Sebastian Fiele
-                    <br />
-                    Von-Kluck-Str. 6
-                    <br />
-                    48151 Münster
+                    Sebastian Fiele<br />
+                    Graelstr. 45<br />
+                    48151 M&uuml;nster
                     <br />
                     E-Mail: rawsta(at)rawsta(.)de
                   </div>
